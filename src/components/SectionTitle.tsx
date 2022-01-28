8412eb3a4d9a7722a8c7f812b8e7ad361bd9c69b
agentgonzo <sarch@cloudbees.com>
2022-01-28 14:12:45 +0000

    Deploying to gh-pages from @ agentgonzo/tutorial-demo-app@d47faa0df0a9dbe9c70646130159f1a2156c37e1 🚀

--- src/components/SectionTitle.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import { CheckIcon } from "./icons/CheckIcon";
-
-export function SectionTitle({done, children}: {done?: boolean, children: ReactNode}) {
-    return <h2 className="sectionTitle">{children} {done && <CheckIcon />}</h2>
-}
